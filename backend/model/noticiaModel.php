c0dec90e4bbbff74867a3abe4db7b861a30259b2
Razawaky <viniciusduarte564@gmail.com>
2024-04-04 08:21:14 -0300

    alteracoes model

--- backend/model/noticiaModel.php
+++ backend/model/noticiaModel.php
@@ -3,15 +3,14 @@
 include ('../connection/conexao.php');
 
 date_default_timezone_set('America/Sao_Paulo');
-$dataAtual = date('Y-m-d H:i:s', time()); //setei o horario e dia padrão, ent estrá já automatico a data
+$dataAtual = date('Y-m-d H:i:s', time()); 
 
 
-//request mesma função do post e get, mas ele ouve os dois
-if($_REQUEST['operacao'] == 'create'){
+if($_POST['operacao'] == 'create'){
 
-    if(empty($_REQUEST['titulo']) || 
-       empty($_REQUEST['resumo']) || 
-       empty($_REQUEST['corpo'])){
+    if(empty($_POST['titulo']) || 
+       empty($_POST['resumo']) || 
+       empty($_POST['corpo'])){
 
         $dados = [
             'type' => 'error',
@@ -21,11 +20,11 @@ if($_REQUEST['operacao'] == 'create'){
 
         try{
             $sql = "INSERT INTO NOTICIA (TITULO, RESUMO, CORPO, DATA) VALUES (?,?,?,?)";
-            $stmt /*statement*/ = $pdo->prepare($sql); //prepare testa o sql conferindo se não há nenhum codigo malicioso
-            $stmt -> execute([ //executa sql
-                $_REQUEST['titulo'],
-                $_REQUEST['resumo'],
-                $_REQUEST['corpo'],
+            $stmt /*statement*/ = $pdo->prepare($sql); 
+            $stmt -> execute([ 
+                $_POST['titulo'],
+                $_POST['resumo'],
+                $_POST['corpo'],
                 $dataAtual
             ]);
             $dados = [
@@ -42,10 +41,86 @@ if($_REQUEST['operacao'] == 'create'){
     }
 }
 
+if($_POST['operacao'] == 'read'){
+    try{
 
-if($_REQUEST['operacao'] == 'read'){}
-if($_REQUEST['operacao'] == 'update'){}
-if($_REQUEST['operacao'] == 'delete'){} 
+        $sql = "SELECT * FROM NOTICIA";
+        $resultado = $pdo->query($sql); 
+        while($row = $resultado->fetch(PDO::FETCH_ASSOC)){ 
+            $dados[] = array_map(null, $row); 
+        }
+
+    }catch(PDOException $e){
+        $dados = [
+            'type' => 'error',
+            'message' => 'Erro de consulta: ' . $e -> getMessage()
+        ];
+    }
+}
+
+if($_POST['operacao'] == 'update'){
+
+    if(empty($_POST['titulo']) || 
+       empty($_POST['resumo']) || 
+       empty($_POST['corpo'])  ||
+       empty($_POST['id'])){
+
+        $dados = [
+            'type' => 'error',
+            'message' => 'Existem campos para alterar vazios.'
+        ];
+    }else{
+
+        try{
+            $sql = "UPDATE NOTICIA SET TITULO = ?, RESUMO = ?, CORPO = ?, DATA = ? WHERE ID = ?";
+            $stmt /*statement*/ = $pdo->prepare($sql); //prepare testa o sql conferindo se não há nenhum codigo malicioso
+            $stmt -> execute([ //executa sql
+                $_POST['titulo'],
+                $_POST['resumo'],
+                $_POST['corpo'],
+                $dataAtual,
+                $_POST['id']
+            ]);
+            $dados = [
+                'type' => 'success',
+                'message' => 'Registro atualizado com sucesso'
+            ];
+        }catch(PDOException $e){
+            $dados = [
+                'type' => 'error',
+                'message' => 'Erro: ' . $e -> getMessage()
+            ];
+        }
+
+    }
+}
+
+if($_REQUEST['operacao'] == 'delete'){
+
+    if(empty($_REQUEST['id'])){
+
+        $dados = [
+            'type' => 'error',
+            'message' => 'ID não reconhecido ou inexistente'
+        ];
+    }else{
+        try{
+            $sql = "DELETE FROM NOTICIA WHERE ID = ?";
+            $stmt /*statement*/ = $pdo->prepare($sql); //prepare testa o sql conferindo se não há nenhum codigo malicioso
+            $stmt -> execute( $_REQUEST['id']);
+            $dados = [
+                'type' => 'success',
+                'message' => 'Registro deletado com sucesso'
+            ];
+        }catch(PDOException $e){
+            $dados = [
+                'type' => 'error',
+                'message' => 'Erro: ' . $e -> getMessage()
+            ];
+        }
+
+    }
+} 
 
 
 echo json_encode($dados);
