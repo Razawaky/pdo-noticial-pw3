9179b9e6a64d744101ef13f4e66103c26b8737ed
Razawaky <viniciusduarte564@gmail.com>
2024-05-18 00:46:11 -0300

    Algumas alterações e adições frontend

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -1,35 +1,42 @@
-document.addEventListener('DOMContentLoaded', function() {
+$(document).ready(function() {
     // Seleciona todos os links da navbar
-    const navLinks = document.querySelectorAll('#navbar ul li a');
+    var navLinks = $('#navbar ul li a');
 
     // Função para adicionar a classe 'active' ao link clicado
     function setActiveLink(event) {
         // Remove a classe 'active' de todos os links
-        navLinks.forEach(link => link.classList.remove('active'));
+        navLinks.removeClass('active');
         // Adiciona a classe 'active' ao link clicado
-        event.target.classList.add('active');
+        $(event.target).addClass('active');
     }
 
     // Adiciona evento de clique a cada link da navbar
-    navLinks.forEach(link => link.addEventListener('click', setActiveLink));
+    navLinks.click(setActiveLink);
 
     // Destaca o link da página atual automaticamente
-    window.onhashchange = function() {
-        navLinks.forEach(link => {
-            if (window.location.hash === link.getAttribute('href')) {
-                link.classList.add('active');
+    $(window).on('hashchange', function() {
+        navLinks.each(function() {
+            if (window.location.hash === $(this).attr('href')) {
+                $(this).addClass('active');
             } else {
-                link.classList.remove('active');
+                $(this).removeClass('active');
             }
         });
-    };
+    });
+
+    // Muda a cor de fundo da navbar ao rolar a página
+    $(window).on('scroll', function() {
+        if ($(window).scrollTop() > 0) { // Se a página foi rolada para baixo
+            $('#navbar').css('backgroundColor', '#000'); 
+        } else {
+            $('#navbar').css('backgroundColor', 'transparent'); // Volta a ser transparente
+        }
+    });
 });
 
-window.addEventListener('scroll', function() {
-    var navbar = document.getElementById('navbar');
-    if (window.pageYOffset > 0) { // Se a página foi rolada para baixo
-        navbar.style.backgroundColor = '#000'; // 
-    } else {
-        navbar.style.backgroundColor = 'transparent'; // Volta a ser transparente
-    }
+// BUTTON HAMBURGUER
+
+$('.menu-btn').click(function() {
+    $(this).toggleClass('open'); /* ABRE O ICONE */
+    $('.menu').slideToggle(); /* MOSTRA OU OCULTA MENU */
 });
